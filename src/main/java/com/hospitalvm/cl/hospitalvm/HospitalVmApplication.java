723f581a9d8452ee727f10984f629fcfb4614f6f
Martin Coronel <coronelmartin@hotmail.cl>
2025-05-06 22:45:02 -0400

    "Creacion proyecto Hospital V&M"

---
+++ src/main/java/com/hospitalvm/cl/hospitalvm/HospitalVmApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalvm.cl.hospitalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalVmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalVmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospitalvm/cl/hospitalvm/HospitalVmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalvm.cl.hospitalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalVmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
